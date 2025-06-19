5ca480efd220f9627080653be61de2bbef4e8f1b
piyu5h25 <113323602+piyu5h25@users.noreply.github.com>
2025-06-19 14:57:49 +0530

    authorization added

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import AuthProvider from './context/AuthContext'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Background from './pages/Background'
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="relative min-h-screen">
+          <Background />
+          <div className="relative z-10">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App
