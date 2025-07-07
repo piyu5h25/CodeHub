d49cbd4b2c2ed32bdae71af443d9cce3df91eaf9
piyu5h25 <113323602+piyu5h25@users.noreply.github.com>
2025-07-07 21:07:05 +0530

    hoh

--- server/index.js
+++ server/index.js
@@ -36,12 +36,5 @@ app.get("/", (req, res) => {
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log(`Access the server at: ${process.env.BACKEND_URL}`);
-    console.log(`Access the login page at: ${process.env.BACKEND_URL}/login`);
-    console.log(`Access the register page at: ${process.env.BACKEND_URL}/register`);
-    console.log(`Access the leaderboard page at: ${process.env.BACKEND_URL}/leaderboard`);
-    console.log(`Access the problems page at: ${process.env.BACKEND_URL}/problems`);
-    console.log(`Access the compiler page at: ${process.env.BACKEND_URL}/compiler`);
-    console.log(`Access the terms page at: ${process.env.BACKEND_URL}/terms`);
-    console.log(`Access the privacy page at: ${process.env.BACKEND_URL}/privacy`);
+   
 });
