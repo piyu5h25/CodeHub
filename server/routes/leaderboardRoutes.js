1c060757026524df4853bf277a772ba2de5de723
piyu5h25 <113323602+piyu5h25@users.noreply.github.com>
2025-07-12 13:16:26 +0530

    authentication not required for leaderboard

--- server/routes/leaderboardRoutes.js
+++ server/routes/leaderboardRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { getLeaderboard } from '../controllers/leaderboardController.js'; 
-import { authenticateToken } from '../middleware/auth.js';  
+// import { authenticateToken } from '../middleware/auth.js';  
 
 const router = express.Router();
 
-router.get('/', authenticateToken, getLeaderboard);
+router.get('/', getLeaderboard);
 
 export default router;
